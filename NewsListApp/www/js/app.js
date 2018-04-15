04e2fdb3d883dc580b42f64e3c5133976fdfbb86
rahul.raviprasad <rahul.raviprasad@gmail.com>
2018-04-15 20:56:36 +0530

    www for a list based app

---
+++ NewsListApp/www/js/app.js
@@ -0,0 +1,60 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var app = angular.module('newsApp', ['ionic']);
+
+app.controller('NewsController', NewsController);
+
+NewsController.$inject = ['$scope', '$http'];
+
+function NewsController($scope, $http) {
+
+  $scope.loadMore = function() {
+    //$http.get('/more-items').success(function(items) {
+      $scope.news.push({
+        title: "psum dolor sit amet, conse",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum"
+      });
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+    //});
+  };
+
+  $scope.news = [
+    {
+      title: "1 dead, over 100 rescued in Nebraska spring blizzard",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum"
+    },
+    {
+      title: "Prime Minister Narendra Modi on Saturday inaugurated the first healthcare and wellness centre under Ayushman",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum"
+    }
+  ];
+
+  // ---
+  // function useItems(items) {
+  //   $scope.news.push()
+  // }
+
+
+}
+
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
